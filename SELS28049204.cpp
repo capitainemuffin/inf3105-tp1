14f979e4e5a6f74d0f60de492fdd03aa47b57991
sofianeselaoui <sofianeselaoui@gmail.com>
2019-06-03 19:40:36 -0400

    commencé aire et perimetre

--- SELS28049204.cpp
+++ SELS28049204.cpp
@@ -5,6 +5,32 @@
 #include <cassert>
 
 /**
+* Représentation d'un point sur les axes x et y
+*/
+class Point {
+
+public:
+
+    double x, y;
+
+    /**
+     * Constructeur par défaut d'un Point, coordonnées (0,0)
+     */
+    Point() : x(0), y(0) {};
+
+    /**
+     * Constructeur qui initialize les coordonnées du Point
+     *
+     * @param x la coordonnée x du Point
+     * @param y la coordonnée y du Point
+     */
+    Point(double _x, double _y) : x(_x), y(_y) {};
+
+    friend std::ostream &operator<<(std::ostream &, Point &);
+
+};
+
+/**
  * Représentation d'un rectangle
  */
 class Rectangle {
@@ -20,45 +46,11 @@ private:
 
     };
 
-    /**
-     * Représentation d'un point du rectangle sur les axes x et y
-     */
-    class Point {
-
-        double x, y;
-
-    public:
-
-        /**
-         * Constructeur par défaut d'un Point, coordonnées (0,0)
-         */
-        Point() : x(0), y(0) {};
-
-        /**
-         * Constructeur qui initialize les coordonnées du Point
-         *
-         * @param x la coordonnée x du Point
-         * @param y la coordonnée y du Point
-         */
-        Point(double _x, double _y) : x(_x), y(_y) {};
-
-        friend std::ostream &operator<<(std::ostream &, Point &);
-
-    };
-
 public:
 
     Type type;
+    double longueur, hauteur;
     Point sup_gauche, sup_droit, inf_gauche, inf_droit;
-
-    /**
-     * Constructeur par défaut d'un rectangle, tous les Points à (0,0)
-     */
-    Rectangle() :
-            sup_gauche(Point()),
-            sup_droit(Point()),
-            inf_gauche(Point()),
-            inf_droit(Point()) {};
 
     /**
      * Constructeur qui initialize les 4 Coins du rectangle
@@ -80,6 +72,21 @@ public:
         this->inf_gauche = Point(x - longueur / 2, y - hauteur / 2);
         this->inf_droit = Point(x + longueur / 2, y - hauteur / 2);
 
+        this->longueur = longueur;
+        this->hauteur = hauteur;
+
+    }
+
+    Rectangle(char type, Point sup_gauche, Point sup_droit, Point inf_gauche, Point inf_droit) {
+
+        this->type = type == 'p' ? Type::positif : Type::negatif;
+        this->sup_gauche = sup_gauche;
+        this->sup_droit = sup_droit;
+        this->inf_gauche = inf_gauche;
+        this->inf_droit = inf_droit;
+
+        this->longueur = this->sup_droit.x - this->sup_gauche.x;
+        this->hauteur = this->sup_gauche.y - inf_gauche.y;
     }
 
     /**
@@ -87,16 +94,60 @@ public:
      *
      * @return true si positif, false sinon
      */
-    bool estPositif() {
+    bool estPositif() const {
         return this->type == Type::positif;
+    }
+
+    /**
+     * Teste si le premier Rectangle est plus petit que le deuxième.
+     *
+     * @param rec1 instance de Rectangle
+     * @param rec2 instance de Rectangle
+     * @return true si plus petit, faux sinon
+     */
+    friend bool operator<(const Rectangle &rec1, const Rectangle &rec2) {
+
+        bool plusPetit = false;
+
+        if (rec1.inf_gauche.x < rec2.inf_gauche.x) {
+
+            plusPetit = true;
+
+        } else if (rec1.inf_gauche.x == rec2.inf_gauche.x && rec1.inf_gauche.y < rec2.inf_gauche.y) {
+
+            plusPetit = true;
+
+        }
+
+        return plusPetit;
+
+    }
+
+    bool croise(const Rectangle &rec2) {
+
+        return
+                this->sup_gauche.x < rec2.inf_droit.x &&
+                this->inf_droit.x > rec2.sup_gauche.x &&
+                this->sup_gauche.y > rec2.inf_droit.y &&
+                this->inf_droit.y < rec2.sup_gauche.y;
+
+    }
+
+    Rectangle croisement(const Rectangle &rec2) {
+
+        double x_gauche = std::max(this->inf_gauche.x, rec2.inf_gauche.x);
+        double x_droit = std::min(this->sup_droit.x, rec2.sup_droit.x);
+        double y_haut = std::min(this->sup_gauche.y, rec2.sup_gauche.y);
+        double y_bas = std::max(this->inf_droit.y, rec2.inf_droit.y);
+
+
+
+        return Rectangle('p', x_gauche, x_droit, x_droit - x_gauche, y_haut - y_bas);
     }
 
     friend std::istream &operator>>(std::ifstream &, Rectangle &);
 
-    friend std::ostream &operator<<(std::ostream &, Point &);
-
     friend std::ostream &operator<<(std::ostream &, Rectangle &);
-
 
 };
 
@@ -107,16 +158,27 @@ class Grille {
 
 public:
 
-    std::vector <Rectangle> rectangles;
+    std::vector <Rectangle> rectangles_positifs;
+    std::vector <Rectangle> rectangles_negatifs;
 
     /**
      * Ajoute le rectangle à la grille
      *
      * @param rectangle
      */
-    void ajouter(Rectangle &rectangle) {
+    void ajouter(const Rectangle &rectangle) {
 
-        this->rectangles.push_back(rectangle);
+        if(rectangle.estPositif()){
+            this->rectangles_positifs.push_back(rectangle);
+            std::sort(rectangles_positifs.begin(), rectangles_positifs.end());
+
+        } else {
+
+            this->rectangles_negatifs.push_back(rectangle);
+            std::sort(rectangles_negatifs.begin(), rectangles_negatifs.end());
+
+        }
+
     }
 
     /**
@@ -127,6 +189,27 @@ public:
     long double aire() {
 
         long double aire = 0;
+
+        for (int i = 0; i < rectangles_positifs.size(); i++) {
+
+            long double aire_a_soustraire = 0;
+            Rectangle rec1 = rectangles_positifs[i];
+
+            for (int j = i+1; j < rectangles_positifs.size(); j++){
+
+                Rectangle rec2 = rectangles_positifs[j];
+
+                if(rec1.croise(rec2)){
+
+                    const Rectangle croisement = rec1.croisement(rec2);
+                    aire_a_soustraire += croisement.longueur * croisement.hauteur;
+
+                }
+            }
+
+            aire += rec1.longueur * rec1.hauteur - aire_a_soustraire;
+        }
+
         return aire;
     }
 
@@ -138,6 +221,27 @@ public:
     double perimetre() {
 
         double perimetre = 0;
+
+        for (int i = 0; i < rectangles_positifs.size(); i++) {
+
+            long double perimetre_a_soustraire = 0;
+            Rectangle rec1 = rectangles_positifs[i];
+
+            for (int j = i+1; j < rectangles_positifs.size(); j++){
+
+                Rectangle rec2 = rectangles_positifs[j];
+
+                if(rec1.croise(rec2)){
+
+                    const Rectangle croisement = rec1.croisement(rec2);
+                    perimetre_a_soustraire += (croisement.longueur * 2) + (croisement.hauteur * 2);
+
+                }
+            }
+
+            perimetre += (rec1.longueur * 2) + (rec1.hauteur * 2) - perimetre_a_soustraire;
+        }
+
         return perimetre;
     }
 
@@ -187,7 +291,7 @@ std::ifstream &validation_fichier(std::ifstream &fichier) {
 
 }
 
-std::ostream &operator<<(std::ostream &os, Rectangle::Point &point) {
+std::ostream &operator<<(std::ostream &os, Point &point) {
 
     os << "(" << point.x << "," << point.y << ")";
 
@@ -215,34 +319,6 @@ std::ostream &operator<<(std::ostream &os, Rectangle &rectangle) {
 }
 
 /**
- * Insère les caractéristiques lues dans le fichier dans l'instance de Rectangle
- *
- * @param is la ligne lue
- * @param rectangle l'instance de Rectangle
- * @return l'instance stream
- */
-std::istream &operator>>(std::ifstream &is, Rectangle &rectangle) {
-
-    if (is) {
-
-        char type;
-        double x, y, longueur, hauteur;
-
-        is >> type >> std::ws;
-        is >> x >> std::ws;
-        is >> y >> std::ws;
-        is >> longueur >> std::ws;
-        is >> hauteur >> std::ws;
-        rectangle = Rectangle(type, x, y, longueur, hauteur);
-
-        std::cout << "X : " << x << " Y : " << y << " L : " << longueur << " H : " << hauteur << std::endl;
-
-    }
-
-    return is;
-}
-
-/**
  * Insère les rectangles du fichier dans la grille
  *
  * @param is le fichier
@@ -253,8 +329,18 @@ std::istream &operator>>(std::ifstream &is, Grille &grille) {
 
     while (!is.eof()) {
 
-        Rectangle rectangle;
-        is >> rectangle;
+        char type;
+        double x, y, longueur, hauteur;
+
+        is >> type >> std::ws;
+        is >> x >> std::ws;
+        is >> y >> std::ws;
+        is >> longueur >> std::ws;
+        is >> hauteur >> std::ws;
+        const Rectangle rectangle = Rectangle(type, x, y, longueur, hauteur);
+
+//        std::cout << "X : " << x << " Y : " << y << " L : " << longueur << " H : " << hauteur << std::endl;
+
         grille.ajouter(rectangle);
     }
 
@@ -273,8 +359,9 @@ int main() {
     fichier >> grille;
     fichier.close();
 
-    for (int i = 0; i < grille.rectangles.size(); i++) {
-        std::cout << grille.rectangles[i] << std::endl;
+    for(int i = 0; i < grille.rectangles_positifs.size(); i++){
+
+        std::cout << grille.rectangles_positifs[i] << std::endl;
     }
 
     std::cout << "aire : " << grille.aire() << std::endl
