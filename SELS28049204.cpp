c3196c14f0d14791f36e943602263c9246a32f49
sofianeselaoui <sofianeselaoui@gmail.com>
2019-06-06 23:32:54 -0400

    area done

--- SELS28049204.cpp
+++ SELS28049204.cpp
@@ -15,25 +15,55 @@ enum class Type {
 
 };
 
+/**
+ * Représentation d'une intervale Y
+ */
 class IntervaleY {
 public:
     double p1, p2;
     Type type;
 
+    /**
+     * Constructeur d'une intervale Y.
+     *
+     * @param type le type positif ou negatif
+     * @param p1 la borne inférieure
+     * @param p2 la borne supérieure
+     */
     IntervaleY(Type type, double p1, double p2) : type(type), p1(p1), p2(p2) {
     }
 
+    /**
+     * Teste si les bornes de l'instance sont identiques à celles de de l'intervale i2.
+     *
+     * @param i2 instance de Intervale
+     * @return True si égal, False sinon
+     */
     bool operator==(const IntervaleY &i2) {
 
         return this->p1 == i2.p1 && this->p2 == i2.p2;
 
     }
 
+    /**
+     * Teste si la borne supérieure de l'intervale Y gauche est inférieure à celle de l'intervale Y droite.
+     *
+     * @param i1
+     * @param i2
+     * @return True si inférieure, False sinon
+     */
     friend bool operator<(const IntervaleY &i1, const IntervaleY &i2) {
 
         return i1.p2 < i2.p2;
     }
 
+    /**
+     * Teste si la borne supérieure de l'intervale Y gauche est supérieure à celle de l'intervale Y droite
+     *
+     * @param i1
+     * @param i2
+     * @return True si supérieure, False sinon
+     */
     friend bool operator>(const IntervaleY &i1, const IntervaleY &i2) {
 
         return i1.p2 > i2.p2;
@@ -41,25 +71,54 @@ public:
 
 };
 
+/**
+ * Représente une intervale X
+ */
 class IntervaleX {
 public :
     double p1, p2;
     std::vector<IntervaleY> intervales_y;
 
+    /**
+     * Constructeur d'une intervale X
+     *
+     * @param p1 la borne à gauche
+     * @param p2 la borne à droite
+     */
     IntervaleX(double p1, double p2) : p1(p1), p2(p2) {
     }
 
+    /**
+     * Teste si les bornes de l'intervale sont identiques à celles de l'intervale i2.
+     *
+     * @param i2 instance d'IntervaleX
+     * @return Vrai si identique, False sinon
+     */
     bool operator==(const IntervaleX &i2) {
 
         return this->p1 == i2.p1 && this->p2 == i2.p2;
 
     }
 
+    /**
+     * Teste si la borne droite de l'intervaleX i1 est inférieure à l'intervale droite de l'intervaleX i2
+     *
+     * @param i1 intervaleX
+     * @param i2 intervaleX
+     * @return Vrai si inférieure, Faux sinon
+     */
     friend bool operator<(const IntervaleX &i1, const IntervaleX &i2) {
 
         return i1.p2 < i2.p2;
     }
 
+    /**
+     * Teste si la borne droite de l'intervaleX i1 est supérieure à l'intervale droite de l'intervaleX i2
+     *
+     * @param i1 intervaleX
+     * @param i2 intervaleX
+     * @return Vrai si supérieure, Faux sinon
+     */
     friend bool operator>(const IntervaleX &i1, const IntervaleX &i2) {
 
         return i1.p2 > i2.p2;
@@ -88,8 +147,6 @@ public:
      * @param y la coordonnée y du Point
      */
     Point(double _x, double _y) : x(_x), y(_y) {};
-
-    friend std::ostream &operator<<(std::ostream &, Point &);
 
 };
 
@@ -126,14 +183,12 @@ public:
     }
 
     /**
-     * Teste si le rectangle est positif ou negatif
+     * Teste si le Rectangle rec2 croise l'instance
      *
-     * @return true si positif, false sinon
+     * @param rec2 Rectangle
+     *
+     * @return Vrai si croise, Faux sinon
      */
-    bool estPositif() const {
-        return this->type == Type::positif;
-    }
-
     bool croise(const Rectangle &rec2) {
 
         return
@@ -143,6 +198,12 @@ public:
                 this->inf_droit.y < rec2.sup_gauche.y;
     }
 
+    /**
+     * Retourne un rectangle qui représente le croisement du rectangle actuel et rec2
+     *
+     * @param rec2 Rectangle
+     * @return Rectangle qui représente le croisement
+     */
     Rectangle croisement(const Rectangle &rec2) {
         double x_gauche = std::max(this->inf_gauche.x, rec2.inf_gauche.x);
         double x_droit = std::min(this->sup_droit.x, rec2.sup_droit.x);
@@ -150,8 +211,6 @@ public:
         double y_bas = std::max(this->inf_droit.y, rec2.inf_droit.y);
         return Rectangle('p', x_gauche, x_droit, x_droit - x_gauche, y_haut - y_bas);
     }
-
-    friend std::ostream &operator<<(std::ostream &, Rectangle &);
 
 };
 
@@ -162,11 +221,17 @@ class Grille {
 
 private:
 
-    std::vector<IntervaleX> get_intervales_x() {
+    /**
+     * Génère des intervales avec tous les points X d'une liste de rectangles.
+     *
+     * @param rectangles liste de rectangles
+     * @return liste d'intervaleX
+     */
+    static std::vector<IntervaleX> get_intervales_x(std::vector<Rectangle> &rectangles) {
 
         std::vector<double> tous_les_x;
 
-        for (auto &rectangle : this->rectangles) {
+        for (auto &rectangle : rectangles) {
 
             double temp = rectangle.inf_gauche.x;
 
@@ -198,11 +263,18 @@ private:
 
     }
 
-    std::vector<IntervaleY> get_intervales_y(IntervaleX intervale_x) {
+    /**
+     * Génére des intervales avec tous les points Y d'une liste de rectangles, à l'intérieurs des intervaleX données.
+     *
+     * @param intervale_x liste d'intervaleX
+     * @param rectangles liste de Rectangles
+     * @return liste d'intervaleY
+     */
+    static std::vector<IntervaleY> get_intervales_y(IntervaleX intervale_x, std::vector<Rectangle> &rectangles) {
 
 
         //Créer les intervales Y
-        for (auto &rectangle : this->rectangles) {
+        for (auto &rectangle : rectangles) {
 
             if (rectangle.inf_gauche.x <= intervale_x.p1 && rectangle.inf_droit.x >= intervale_x.p2) {
                 //ce rectangle est dans cet intervale, mettre les intervales y1 et y2
@@ -229,29 +301,34 @@ private:
 
         //fusionner les intervalles y qui se chevauchent ou qui sont identiques
         std::vector<IntervaleY> fusions_y;
-
         for (int i = intervale_x.intervales_y.size() - 1; i >= 0; i--) {
 
-            double min = intervale_x.intervales_y[i].p1;
-            double max = intervale_x.intervales_y[i].p2;
+            Type type_i = intervale_x.intervales_y[i].type;
+            double min_i = intervale_x.intervales_y[i].p1;
+            double max_i = intervale_x.intervales_y[i].p2;
 
             int j = i - 1;
 
             while (j >= 0) {
 
-                if (min <= intervale_x.intervales_y[j].p2 && min >= intervale_x.intervales_y[j].p1) {
+                Type type_j = intervale_x.intervales_y[j].type;
+                double min_j = intervale_x.intervales_y[j].p1;
+                double max_j = intervale_x.intervales_y[j].p2;
 
-                    min = intervale_x.intervales_y[j].p1;
+
+                if (min_i <= max_j && min_i >= min_j) {
+
+                    min_i = min_j;
                     i--;
 
-                } else if (min <= intervale_x.intervales_y[j].p1 && max >= intervale_x.intervales_y[j].p2) {
+                } else if (min_i <= min_j && max_i >= max_j) {
                     i--;
                 }
 
                 j--;
             }
 
-            IntervaleY int_y = IntervaleY(Type::positif, min, max);
+            IntervaleY int_y = IntervaleY(intervale_x.intervales_y[i].type, min_i, max_i);
 
             //vérifier que l'intervale fusionnée ne se trouve pas déjà dans la liste
             if (!(std::find(fusions_y.begin(), fusions_y.end(), int_y) != fusions_y.end())) {
@@ -267,56 +344,98 @@ private:
 
 public:
 
-    std::vector<Rectangle> rectangles;
+    std::vector<Rectangle> rectangles_positifs;
+    std::vector<Rectangle> rectangles_negatifs;
 
+    /**
+     * Fractionne les rectangles de la grille selon des intervales X et Y.
+     */
     void fractionner_rectangles() {
 
-        std::vector<IntervaleX> intervales_x = get_intervales_x();
+        //fractionner les rectangles positifs
+        if (!rectangles_positifs.empty()) {
 
-        for (auto &intervale_x : intervales_x) {
-            intervale_x.intervales_y = get_intervales_y(intervale_x);
-        }
-
-        std::vector<Rectangle> rectangles_fractionnes;
-        for (auto &intervale_x : intervales_x) {
-            for (auto &intervale_y : intervale_x.intervales_y) {
-
-                double longueur = intervale_x.p2 - intervale_x.p1;
-                double hauteur = intervale_y.p2 - intervale_y.p1;
-                double x = intervale_x.p1 + longueur / 2;
-                double y = intervale_y.p1 + hauteur / 2;
-                char type = intervale_y.type == Type::positif ? 'p' : 'n';
-                rectangles_fractionnes.__emplace_back(Rectangle(type, x, y, longueur, hauteur));
+            std::vector<IntervaleX> intervales_x_positifs = get_intervales_x(this->rectangles_positifs);
+            for (auto &intervale_x : intervales_x_positifs) {
+                intervale_x.intervales_y = get_intervales_y(intervale_x, this->rectangles_positifs);
             }
+
+            std::vector<Rectangle> rectangles_fractionnes_positifs;
+            for (auto &intervale_x : intervales_x_positifs) {
+                for (auto &intervale_y : intervale_x.intervales_y) {
+
+                    double longueur = intervale_x.p2 - intervale_x.p1;
+                    double hauteur = intervale_y.p2 - intervale_y.p1;
+                    double x = intervale_x.p1 + longueur / 2;
+                    double y = intervale_y.p1 + hauteur / 2;
+                    char type = intervale_y.type == Type::positif ? 'p' : 'n';
+                    rectangles_fractionnes_positifs.__emplace_back(Rectangle(type, x, y, longueur, hauteur));
+                }
+            }
+
+            this->rectangles_positifs = rectangles_fractionnes_positifs;
+
         }
 
-        this->rectangles = rectangles_fractionnes;
+        //fractionner les rectangles negatifs
+        if (!rectangles_negatifs.empty()) {
+
+            std::vector<IntervaleX> intervales_x_negatifs = get_intervales_x(this->rectangles_negatifs);
+            for (auto &intervale_x : intervales_x_negatifs) {
+                intervale_x.intervales_y = get_intervales_y(intervale_x, this->rectangles_negatifs);
+            }
+
+            std::vector<Rectangle> rectangles_fractionnes_negatifs;
+            for (auto &intervale_x : intervales_x_negatifs) {
+                for (auto &intervale_y : intervale_x.intervales_y) {
+
+                    double longueur = intervale_x.p2 - intervale_x.p1;
+                    double hauteur = intervale_y.p2 - intervale_y.p1;
+                    double x = intervale_x.p1 + longueur / 2;
+                    double y = intervale_y.p1 + hauteur / 2;
+                    char type = intervale_y.type == Type::positif ? 'p' : 'n';
+                    rectangles_fractionnes_negatifs.__emplace_back(Rectangle(type, x, y, longueur, hauteur));
+                }
+            }
+
+            this->rectangles_negatifs = rectangles_fractionnes_negatifs;
+
+        }
 
     }
 
     /**
-     * Calcule l'aire de tous les rectangles
+     * Retourne l'aire de la forme sur la grille
      *
      * @return aire
      */
     long double aire() {
 
         fractionner_rectangles();
-        //calculer l'aire
         long double aire = 0;
 
-        for (const auto &rectangle : this->rectangles) {
+        for (auto &rectangle_p : this->rectangles_positifs) {
 
-            aire += (rectangle.sup_droit.y - rectangle.inf_droit.y) * (rectangle.inf_droit.x - rectangle.inf_gauche.x);
+            aire += (rectangle_p.sup_droit.y - rectangle_p.inf_droit.y) *
+                    (rectangle_p.inf_droit.x - rectangle_p.inf_gauche.x);
+
+            for (auto &rectangles_n : this->rectangles_negatifs) {
+
+                if (rectangle_p.croise(rectangles_n)) {
+
+                    Rectangle intersection = rectangle_p.croisement(rectangles_n);
+                    aire -= (intersection.sup_droit.y - intersection.inf_droit.y) *
+                            (intersection.inf_droit.x - intersection.inf_gauche.x);
+                }
+            }
         }
-
 
         return aire;
 
     }
 
     /**
-     * Calcule le perimètre de tous les rectangles
+     * Retourne le perimètre de la forme sur la grille
      *
      * @return perimetre
      */
@@ -374,33 +493,6 @@ std::ifstream &validation_fichier(std::ifstream &fichier) {
 
 }
 
-std::ostream &operator<<(std::ostream &os, Point &point) {
-
-    os << "(" << std::setprecision(16) << point.x << "," << point.y << ")";
-
-    return os;
-}
-
-std::ostream &operator<<(std::ostream &os, Rectangle &rectangle) {
-
-    if (rectangle.estPositif()) {
-
-        os << "Rectangle positif" << std::endl;
-
-    } else {
-
-        os << "Rectangle negatif" << std::endl;
-    }
-
-    os << "Supérieur gauche : " << std::setprecision(16) << rectangle.sup_gauche << std::endl;
-    os << "Supérieur droite : " << std::setprecision(16) << rectangle.sup_droit << std::endl;
-    os << "Inférieur gauche : " << std::setprecision(16) << rectangle.inf_gauche << std::endl;
-    os << "Inférieur droit : " << std::setprecision(16) << rectangle.inf_droit << std::endl;
-
-    return os;
-
-}
-
 /**
  * Insère les rectangles du fichier dans la grille
  *
@@ -425,7 +517,14 @@ std::istream &operator>>(std::ifstream &is, Grille &grille) {
         std::cout << "X : " << x << " Y : " << y << " L : " << std::setprecision(16) << longueur << " H : "
                   << std::setprecision(16) << hauteur << std::endl;
 
-        grille.rectangles.push_back(rectangle);
+        if (rectangle.type == Type::positif) {
+
+            grille.rectangles_positifs.push_back(rectangle);
+
+        } else {
+
+            grille.rectangles_negatifs.push_back(rectangle);
+        }
     }
 
     return is;
@@ -442,11 +541,6 @@ int main() {
     Grille grille = Grille();
     fichier >> grille;
     fichier.close();
-
-    for (int i = 0; i < grille.rectangles.size(); i++) {
-
-        std::cout << grille.rectangles[i] << std::endl;
-    }
 
     std::cout << "aire : " << std::setprecision(16) << grille.aire() << std::endl
               << "perimetre : " << std::setprecision(16) << grille.perimetre() << std::endl;
